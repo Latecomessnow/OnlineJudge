015694ac89fe4e3a44548f4d2395a0ad8690ffe5
15978104402 <guhui4402@foxmail.com>
2023-05-09 17:51:29 +0800

    OJ

---
+++ Common/log.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include "util.hpp"
+
+namespace ns_log
+{
+    using namespace ns_util;
+    // 日志等级
+    enum
+    {
+        INFO, // 就是整数
+        DEBUG,
+        WARNING,
+        ERROR,
+        FATAL
+    };
+    // 让日志返回标准输出流，以便于自定义输出
+    std::ostream &Log(const std::string &level, const std::string &file_name, int line)
+    {
+        // 1. 添加日志等级
+        std::string message = "[";
+        message += level;
+        message += "]";
+
+        // 2. 添加报错文件名
+        message += "[";
+        message += file_name;
+        message += "]";
+
+        // 3. 添加报错行
+        message += "[";
+        message += std::to_string(line);
+        message += "]";
+
+        // 4. 添加日志时间
+        message += "[";
+        message += "待定";
+        message += "]";
+        return std::cout;
+    }
+    // Log() << "message"  // 待#号可以直接传过去的是字符，而不是整数了
+    // 开放式日志接口
+    #define LOG(level) Log(#level, __FILE__, __LINE__)
+}
--- Common/util.hpp
+++ Common/util.hpp
@@ -1,25 +1,67 @@
 #pragma once
 
 #include <iostream>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <string>
 
 // 编译模块，运行模块都有可能会用上的添加后缀名...
 namespace ns_util
 {
+    const std::string temp_path = "./temp/";
+    // 文件路径相关的
     class PathUtil
     {
     public:
+        // 123 -> ./temp/123.cpp
+        static std::string AddSuffix(const std::string &file_name, const std::string &suffix)
+        {
+            std::string path_name = temp_path;
+            path_name += file_name;
+            path_name += suffix;
+            return path_name;
+        }
         // 构建源文件路径+后缀的完整文件名
         static std::string Src(const std::string &file_name)
         {
+            return AddSuffix(file_name, ".cpp");
         }
         // 构建可执行程序的完整路径+后缀名
         static std::string Exe(const std::string &file_name)
         {
+            return AddSuffix(file_name, ".exe");
         }
         // 构建该程序对应的标准错误的完整路径+后缀名
         static std::string Stderr(const std::string &file_name)
         {
+            return AddSuffix(file_name, ".stderr");
+        }
+    };
+
+    // 文件相关的
+    class FileUtil
+    {
+    public:
+        static bool IsFileExists(const std::string &path_name)
+        {
+            struct stat st;
+            if (stat(path_name.c_str(), &st) == 0)
+            {
+                // stat函数是获取某个路径下的文件属性，如果获取文件属性成功则返回0,表明文件存在
+                return true;
+            }
+            return false;
+        }
+    };
+
+    // 时间功能
+    class TimeUtil
+    {
+    public:
+        static std::string GetTimeStamp()
+        {
+            
         }
     };
 } // namespace ns_util
