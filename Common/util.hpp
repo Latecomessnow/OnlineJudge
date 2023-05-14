0714ad030576b88e53d23768c16f2033c661c6c1
15978104402 <guhui4402@foxmail.com>
2023-05-14 23:30:02 +0800

    OJ

--- Common/util.hpp
+++ Common/util.hpp
@@ -1,16 +1,41 @@
 #pragma once
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <atomic>
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/time.h>
-#include <string>
 
 // 编译模块，运行模块都有可能会用上的添加后缀名...
 namespace ns_util
 {
     const std::string temp_path = "./temp/";
+
+    // 时间功能
+    class TimeUtil
+    {
+    public:
+        static std::string GetTimeStamp()
+        {
+            // 调用gettimeofday获取时间戳
+            struct timeval _time;
+            gettimeofday(&_time, nullptr); // 设为nullptr表示要调用gettimeofday
+            // 返回时间戳秒数
+            return std::to_string(_time.tv_sec);
+        }
+
+        // 获取毫秒级时间戳
+        static std::string GetTimeMs()
+        {
+            struct timeval _time;
+            gettimeofday(&_time, nullptr);
+            return std::to_string(_time.tv_sec * 1000 + _time.tv_usec / 1000);
+        }
+    };
+
     // 文件路径相关的
     class PathUtil
     {
@@ -72,30 +97,39 @@ namespace ns_util
         }
         static std::string UniqFileName()
         {
-            // TODO
-            return "";
+            // 毫秒级时间戳+原子性递增唯一值: 确保文件名唯一性
+            static std::atomic_uint id(0);
+            // 原子性++
+            id++;
+            std::string ms = TimeUtil::GetTimeMs();
+            std::string uniq_id = std::to_string(id);
+            return ms + "_" + uniq_id;
         }
-        static bool WritweFile(const std::string& file_name, const std::string& code)
+        static bool WriteFile(const std::string &target, const std::string &code)
         {
-
+            std::ofstream out(target);
+            if (!out.is_open())
+                return false;
+            out.write(code.c_str(), code.size());
+            out.close();
+            return true;
         }
-        static std::string ReadFile(const std::string& file_name)
+        static bool ReadFile(const std::string &target, std::string *content, bool keep = false)
         {
-            return 
-        }
-    };
-
-    // 时间功能
-    class TimeUtil
-    {
-    public:
-        static std::string GetTimeStamp()
-        {
-            // 调用gettimeofday获取时间戳
-            struct timeval _time;
-            gettimeofday(&_time, nullptr); // 设为nullptr表示要调用gettimeofday
-            // 返回时间戳秒数
-            return std::to_string(_time.tv_sec);
+            (*content).clear();
+            std::ifstream in(target);
+            if (!in.is_open())
+                return false;
+            std::string line;
+            // getline按行读取不保存分隔符，如\n
+            // getline内部重载了强制类型转换
+            while (std::getline(in, line))
+            {
+                (*content) += line;
+                (*content) += (keep ? "\n" : "");
+            }
+            in.close();
+            return true;
         }
     };
 } // namespace ns_util
