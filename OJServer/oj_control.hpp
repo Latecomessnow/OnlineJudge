f6cedd3fd632270ce770043b194e1434bf09a2e9
15978104402 <guhui4402@foxmail.com>
2023-05-19 22:23:07 +0800

    OJ

--- OJServer/oj_control.hpp
+++ OJServer/oj_control.hpp
@@ -6,17 +6,19 @@
 #include "../Common/util.hpp"
 #include "../Common/log.hpp"
 #include "oj_model.hpp"
+#include "oj_view.hpp"
 
 namespace ns_control
 {
     using namespace ns_util;
     using namespace ns_log;
     using namespace ns_model;
-
+    using namespace ns_view;
     class Control
     {
     private:
-        Model _model;
+        Model _model; // 提供题目数据
+        View _view;   // 将题目数据渲染成网页
     public:
         Control(){}
         ~Control(){}
@@ -25,29 +27,35 @@ namespace ns_control
         // html: 输出型参数
         bool AllQuestions(std::string *html)
         {
+            bool ret = true;
             std::vector<struct Question> all;
             if (_model.GetAllQuestions(&all))
             {
                 // 获取所有题目信息成功，将所有的题目构建成网页
-
+                _view.AllExpandHtml(all, html);
             }
             else
             {
-
+                *html = "获取题目列表失败";
+                ret = false;
             }
+            return ret;
         }
         bool Question(const std::string &number, std::string *html)
         {
             struct Question q;
+            bool ret = true;
             if (_model.GetOneQuestion(number, &q))
             {
                 // 获取指定题目信息成功，将所有的题目构建成网页
-
+                _view.OneExpandHtml(q, html);
             }
             else
             {
-
+                *html = "指定题目: " + number + "不存在!";
+                ret = false;
             }
+            return ret;
         }
     };
 }
--- OJServer/oj_view.hpp
+++ OJServer/oj_view.hpp
@@ -1,1 +1,47 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "oj_model.hpp"
+#include <ctemplate/template.h>
+namespace ns_view
+{
+    using namespace ns_model;
+    const std::string template_path = "./template_html/";
+    class View
+    {
+    public:
+        View(){}
+        ~View(){}
+    public:
+        bool AllExpandHtml(const std::vector<struct Question> &questions, std::string *html)
+        {
+            // 题目的编号 题目的标题 题目的难度
+            // 推荐使用表格显示
+            // 1. 形成待渲染文件的路径
+            std::string src_html = template_path + "all_questions.html";
+            // 2. 形成字典
+            ctemplate::TemplateDictionary root("all_questions");
+            for (const auto& q : questions)
+            {
+                // 添加子字典
+                ctemplate::TemplateDictionary *sub = root.AddSectionDictionary("questions_list");
+                sub->SetValue("number", q.number);
+                sub->SetValue("title", q.title);
+                sub->SetValue("star", q.star);
+            }
+            
+            // 3. 获取被渲染的html                                                    // 不对html做任何的删除
+            ctemplate::Template *tpl = ctemplate::Template::GetTemplate(src_html, ctemplate::DO_NOT_STRIP)
+
+            // 4. 开始完成渲染功能
+            tpl->Expand(html, &root);
+            return true;
+        }
+        bool OneExpandHtml(const struct Question &q, std::string *html)
+        {
+            return true;
+        }
+    };
+}
