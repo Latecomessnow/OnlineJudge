53919688878b608bd375ea56c0abc790e9e4410f
15978104402 <guhui4402@foxmail.com>
2023-05-22 23:19:33 +0800

    OJ

--- OJServer/oj_control.hpp
+++ OJServer/oj_control.hpp
@@ -11,6 +11,7 @@
 #include "../Common/log.hpp"
 #include "oj_model.hpp"
 #include "oj_view.hpp"
+#include "../Common/httplib.h"
 
 namespace ns_control
 {
@@ -18,6 +19,7 @@ namespace ns_control
     using namespace ns_log;
     using namespace ns_model;
     using namespace ns_view;
+    using namespace httplib;
 
     // 提供编译服务的主机
     class Machine
@@ -28,32 +30,39 @@ namespace ns_control
         uint64_t load;   // 编译服务的负载数
         std::mutex *mtx; // mutex对象是禁止拷贝的，所以此处使用指针去拷贝
     public:
-        Machine() : ip(""), port(0), load(0), mtc(nullptr)
+        Machine() : ip(""), port(0), load(0), mtx(nullptr)
         {
         }
         ~Machine() {}
+
     public:
         // 提升主机负载
         void IncLoad()
         {
-            if (mtx) mtx->lock();
+            if (mtx)
+                mtx->lock();
             load++;
-            if (mtx) mtx->unlock();
+            if (mtx)
+                mtx->unlock();
         }
         // 降低主机负载
         void DecLoad()
         {
-            if (mtx) mtx->lock();
+            if (mtx)
+                mtx->lock();
             load--;
-            if (mtx) mtx->unlock();
+            if (mtx)
+                mtx->unlock();
         }
         // 获取主机负载，意义不大只是为了统一接口
         uint64_t Load()
         {
             uint64_t _load = 0;
-            if (mtx) mtx->lock();
+            if (mtx)
+                mtx->lock();
             _load = load;
-            if (mtx) mtx->unlock();
+            if (mtx)
+                mtx->unlock();
             return _load;
         }
     };
@@ -71,6 +80,7 @@ namespace ns_control
         std::vector<int> offline;
         // 保护LoadBlance数据
         std::mutex mtx;
+
     public:
         LoadBlance()
         {
@@ -79,11 +89,12 @@ namespace ns_control
                       << "\n";
         }
         ~LoadBlance() {}
+
     public:
         bool LoadConf(const std::string &machine_conf)
         {
             std::ifstream in(machine_conf);
-            if (!in.open())
+            if (!in.is_open())
             {
                 LOG(FATAL) << " 加载 " << machine_conf << " 文件失败 "
                            << "\n";
@@ -134,35 +145,71 @@ namespace ns_control
             }
             // 遍历找到最小负载的主机
             *id = online[0];
-            *m = machines[online[0]];
+            *m = &machines[online[0]];
             uint64_t min_load = machines[online[0]].Load();
-            for (int i = 0; i < online_num; i++)
+            for (int i = 1; i < online_num; i++)
             {
                 uint64_t curr_load = machines[online[i]].Load();
                 if (min_load > curr_load)
                 {
                     min_load = curr_load;
                     *id = online[i];
-                    *m = machines[online[i]];   
+                    *m = &machines[online[i]];
                 }
             }
-            mtc.unlock();
+            mtx.unlock();
             return true;
         }
         void OnlineMachine()
         {
+            
         }
-        void OfflineMachine()
+        void OfflineMachine(int which)
         {
+            // 在离线的同时有可能有用户正在进行SmartChoice，所以需要加锁保护
+            mtx.lock();
+            for (auto iter = online.begin(); iter != online.end(); iter++)
+            {
+                // 找到离线主机
+                if (*iter == which)
+                {
+                    online.erase(iter);
+                    // offline.push_back(*iter); // 迭代器已经生效，不能再写*iter
+                    offline.push_back(which);
+                    // 因为有break的存在，一旦找到就会跳出了，所以暂时不考虑erase之后迭代器失效的问题
+                    break;
+                }
+            }
+            mtx.unlock();
+        }
+        // for test function
+        void ShowMachines()
+        {
+            mtx.lock();
+            std::cout << "当前在线主机列表: ";
+            for (auto &id : online)
+            {
+                std::cout << id << " ";
+            }
+            std::cout << std::endl;
+
+            std::cout << "当前离线主机列表: ";
+            for (auto &id :offline)
+            {
+                std::cout << id << " ";
+            }
+            std::cout << std::endl;
+            
+            mtx.unlock();
         }
     };
     // 核心业务逻辑的控制器
     class Control
     {
     private:
-        Model _model;               // 提供题目数据
-        View _view;                 // 将题目数据渲染成网页
-        LoadBlance _load_blance;    // 核心负载均衡器
+        Model _model;            // 提供题目数据
+        View _view;              // 将题目数据渲染成网页
+        LoadBlance _load_blance; // 核心负载均衡器
     public:
         Control() {}
         ~Control() {}
@@ -226,10 +273,41 @@ namespace ns_control
             // 将形成的这个具有完整代码和时空要求的代码转成json串数据发送给compile_server服务
             Json::FastWriter writer;
             std::string compile_string = writer.write(compile_value);
-            
+
             // 3. oj_control选择负载最小的主机(差错处理)
-            // 4. 发起http请求，得到结果
-            // 5. 将结果赋值给out_json
+            // 规则: 一直选择直到有可用的主机，否则就是所有主机全部都挂掉了
+            while (true)
+            {
+                int id = 0;
+                Machine *m = nullptr;
+                if (!_load_blance.SmartChoice(&id, &m))
+                {
+                    break;
+                }
+                // 4. 发起http请求，得到结果
+                Client cli(m->ip, m->port);
+                m->IncLoad();
+                LOG(INFO) << " 选择主机成功, 主机id: " << id << " 详情: " << m->ip << ":" << m->port << "\n";
+                if (auto res = cli.Post("/compile_and_run", compile_string, "application/json;charset=utf-8"))
+                {
+                    // 有可能会出现请求失败
+                    if (res->status == 200)
+                    {
+                        // 5. 将结果赋值给out_json
+                        *out_json = res->body;
+                        m->DecLoad();
+                        LOG(INFO) << "请求编译和运行服务成功" << "\n";
+                        break;
+                    }
+                }
+                else
+                {
+                    LOG(ERROR) << "当前请求的主机id " << id << " 详情: " << m->ip << ":" << m->port << "可能已经挂掉了"
+                               << "\n";
+                    _load_blance.OfflineMachine(id);
+                    _load_blance.ShowMachines(); // 仅仅用来测试
+                }
+            }
         }
     };
 }
