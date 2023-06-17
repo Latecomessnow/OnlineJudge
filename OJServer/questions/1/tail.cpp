b96a42d8114803861278dcf2bb067bee0a65bfad
15978104402 <guhui4402@foxmail.com>
2023-06-17 09:17:45 +0800

    判断回文数

--- OJServer/questions/1/tail.cpp
+++ OJServer/questions/1/tail.cpp
@@ -6,14 +6,18 @@ void Test1()
 {
     bool ret = Solution().isPalindrome(121);
     if (ret) std::cout << "通过测试用例1, 测试121通过 ... OK!" << std::endl;
-    else std::cout << "未通过测试用例1, 用例1测试值是121" << std::endl;
+    // else std::cout << "未通过测试用例1, 用例1测试值是121" << std::endl;
+    else std::cout << "Wrong Answer!" << std::endl;
 }
+
 void Test2()
 {
     bool ret = Solution().isPalindrome(-121);
     if (ret) std::cout << "通过测试用例2, 测试-121通过 ... OK!" << std::endl;
-    else std::cout << "未通过测试用例1, 用例1测试值是-121" << std::endl;
+    // else std::cout << "未通过测试用例1, 用例1测试值是-121" << std::endl;
+    else std::cout << "Wrong Answer!" << std::endl;
 }
+
 int main()
 {
     Test1();
